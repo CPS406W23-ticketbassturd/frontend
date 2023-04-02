4ac0200abec60085075755cf805274c13b7e97d8
Brandon Ly <git@brndn.ly>
2023-04-01 20:57:35 -0400

    fix: colouring on search bar

--- pages/index.jsx
+++ pages/index.jsx
@@ -13,9 +13,9 @@ import * as React from "react";
 const Search = styled('div')(({ theme }) => ({
     position: 'relative',
     borderRadius: theme.shape.borderRadius,
-    backgroundColor: alpha(theme.palette.common.white, 0.2),
+    backgroundColor: alpha(theme.palette.common.black, 0.1),
     '&:hover': {
-        backgroundColor: alpha(theme.palette.common.white, 0.25),
+        backgroundColor: alpha(theme.palette.common.black, 0.25),
     },
     marginLeft: 0,
     width: '100%',
